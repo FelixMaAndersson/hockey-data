ff4cf1f6c9ab15270eba63f4371cc3ff3d0b8a62
Felix Andersson <felix.andersson@gmail.com>
2026-04-29 12:39:46 +0200

    started with the variables

--- src/main/java/se/yrgo/domain/Player.java
+++ src/main/java/se/yrgo/domain/Player.java
@@ -3,10 +3,19 @@ package se.yrgo.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-
 public class Player {
+
+    private String playerId;
+    private String fullName;
+    private String teamName;
+    private String position;
+    private String jerseyNr; // should this be an int?
+    private String teamLeader;
+    private int refereeHeckling;
+    private int beerChugging;
+    private int diving;
+    private int
+    private int salary;
 
 	// needed for JPA - ignore until then
 	public Player() {}
