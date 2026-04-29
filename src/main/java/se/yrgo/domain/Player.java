831c48e27c1b221eeaee6cca1e2db3e4c89494bb
Felix Andersson <felix.andersson@gmail.com>
2026-04-29 14:17:12 +0200

    starting variables for player

--- src/main/java/se/yrgo/domain/Player.java
+++ src/main/java/se/yrgo/domain/Player.java
@@ -10,11 +10,13 @@ public class Player {
     private String teamName;
     private String position;
     private String jerseyNr; // should this be an int?
-    private String teamLeader;
+    private String captain; // in teams?
     private int refereeHeckling;
     private int beerChugging;
     private int diving;
-    private int
+    private int game;
+    private int snusing;
+    private int swag;
     private int salary;
 
 	// needed for JPA - ignore until then
