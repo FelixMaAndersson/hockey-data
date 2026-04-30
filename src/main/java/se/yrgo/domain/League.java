2ed82e2240bdc91b833f5b7cea84b627fa2ff000
Gustav O <ac75309@skola.goteborg.se>
2026-04-30 11:44:51 +0200

    Added league entity

---
+++ src/main/java/se/yrgo/domain/League.java
@@ -0,0 +1,45 @@
+package se.yrgo.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class League {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    @OneToMany(mappedBy = "league", cascade = CascadeType.ALL)
+    private List<Team> teams = new ArrayList<>();
+
+    public League() {
+    }
+
+    public League(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Team> getTeams() {
+        return this.teams;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return "League{" +
+                "name='" + name + '\'' +
+                ", teams=" + teams +
+                '}';
+    }
+}
