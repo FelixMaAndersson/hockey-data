94774942a7737f54f300b1d80b57d6748744c4c1
Felix Andersson <felix.andersson@gmail.com>
2026-05-06 13:21:45 +0200

    fleshed out methods in implementation

--- src/main/java/se/yrgo/exceptions/PlayerNotFoundException.java
+++ src/main/java/se/yrgo/exceptions/PlayerNotFoundException.java
@@ -2,9 +2,8 @@ package se.yrgo.exceptions;
 
 public class PlayerNotFoundException extends Exception {
 
-	/**
-	 * Just to stop warnings.
-	 */
-	private static final long serialVersionUID = 1L;
+    public PlayerNotFoundException(String playerId) {
+        super("Player with id " + playerId + " was not found");
+    }
 
 }
