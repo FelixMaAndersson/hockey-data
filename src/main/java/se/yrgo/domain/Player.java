df85fc3a41256b98393b51a54fb5cb19c8200e69
Felix Andersson <felix.andersson@gmail.com>
2026-05-06 12:52:45 +0200

    finished w player domain

--- src/main/java/se/yrgo/domain/Player.java
+++ src/main/java/se/yrgo/domain/Player.java
@@ -1,16 +1,26 @@
 package se.yrgo.domain;
 
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+@Entity
 public class Player {
 
+    @Id
     private String playerId;
+
     private String fullName;
-    private Team team;
+
+    @ManyToMany(mappedBy = "players")
+    private List<Team> teams = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
     private Position position;
+
     private int jerseyNr;
-    private boolean captain;
 
     // stats / ratings
     private int refereeHeckling;
@@ -19,10 +29,98 @@ public class Player {
     private int game;
     private int snusing;
     private int swag;
+
     private int salary;
 
+    public Player() {
+    }
 
-	// needed for JPA - ignore until then
-	public Player() {}
+    public Player(String playerId, String fullName, Position position, int jerseyNr,
+                  int refereeHeckling, int beerChugging, int diving,
+                  int game, int snusing, int swag, int salary) {
 
+        this.playerId = playerId;
+        this.fullName = fullName;
+        this.position = position;
+        this.jerseyNr = jerseyNr;
+        this.refereeHeckling = refereeHeckling;
+        this.beerChugging = beerChugging;
+        this.diving = diving;
+        this.game = game;
+        this.snusing = snusing;
+        this.swag = swag;
+        this.salary = salary;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public int getJerseyNr() {
+        return jerseyNr;
+    }
+
+    public int getRefereeHeckling() {
+        return refereeHeckling;
+    }
+
+    public int getBeerChugging() {
+        return beerChugging;
+    }
+
+    public int getDiving() {
+        return diving;
+    }
+
+    public int getGame() {
+        return game;
+    }
+
+    public int getSnusing() {
+        return snusing;
+    }
+
+    public int getSwag() {
+        return swag;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "playerId='" + playerId + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", position=" + position +
+                ", jerseyNr=" + jerseyNr +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Player player)) {
+            return false;
+        }
+        return Objects.equals(playerId, player.playerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerId);
+    }
 }
