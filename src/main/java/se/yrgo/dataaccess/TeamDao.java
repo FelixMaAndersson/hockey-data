f747d9789ffa721fc55ff7eae347d9d2b8a9749b
Felix Andersson <felix.andersson@gmail.com>
2026-05-06 13:14:43 +0200

    fixed merge conflict

--- src/main/java/se/yrgo/dataaccess/TeamDao.java
+++ src/main/java/se/yrgo/dataaccess/TeamDao.java
@@ -1,11 +1,25 @@
 package se.yrgo.dataaccess;
 
 import se.yrgo.domain.Team;
-import se.yrgo.exceptions.PlayerNotFoundException;
+import se.yrgo.domain.Player;
+import se.yrgo.exceptions.TeamNotFoundException;
+
+import java.util.List;
 
 public interface TeamDao {
-	public void create(Team newTeam);
-	// public List<Team> getIncompleteTeams(String userId);
-	public void update(Team teamToUpdate) throws PlayerNotFoundException;
-	public void delete(Team oldTeam) throws PlayerNotFoundException;
+
+    void create(Team team);
+
+    Team getById(long teamId) throws TeamNotFoundException;
+
+    Team getByName(String name) throws TeamNotFoundException;
+
+    void update(Team team) throws TeamNotFoundException;
+
+    void delete(Team team) throws TeamNotFoundException;
+
+    List<Team> getAllTeams();
+
+    List<Player> getAllPlayers(long teamId) throws TeamNotFoundException;
 }
+
