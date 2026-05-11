ea4f205b4307d146abdd067e6189d86cf22e3c0b
Felix Andersson <felix.andersson@gmail.com>
2026-05-11 14:09:32 +0200

    removed debug print from SimpleClient

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -11,7 +11,7 @@ import java.sql.*;
 
 public class SimpleClient {
     public static void main(String[] args) {
-        printAllTables();
+
         try (ClassPathXmlApplicationContext container =
                      new ClassPathXmlApplicationContext("application.xml")) {
 
@@ -19,53 +19,6 @@ public class SimpleClient {
             menu.start();
         } catch (LeagueNotFoundException | TeamNotFoundException | PlayerNotFoundException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private static void printAllTables() {
-
-        String url = "jdbc:hsqldb:file:database.dat";
-        String user = "sa";
-        String password = "";
-
-        try (
-                Connection conn = DriverManager.getConnection(url, user, password);
-                Statement stmt = conn.createStatement()
-        ) {
-
-            ResultSet tables = stmt.executeQuery(
-                    "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES " +
-                            "WHERE TABLE_SCHEMA = 'PUBLIC'"
-            );
-
-            while (tables.next()) {
-
-                String tableName = tables.getString("TABLE_NAME");
-
-                System.out.println("\n===== " + tableName + " =====");
-
-                ResultSet rows = stmt.executeQuery("SELECT * FROM " + tableName);
-
-                ResultSetMetaData meta = rows.getMetaData();
-                int columns = meta.getColumnCount();
-
-                while (rows.next()) {
-
-                    for (int i = 1; i <= columns; i++) {
-                        System.out.print(
-                                meta.getColumnName(i)
-                                        + ": "
-                                        + rows.getString(i)
-                                        + "   "
-                        );
-                    }
-
-                    System.out.println();
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
