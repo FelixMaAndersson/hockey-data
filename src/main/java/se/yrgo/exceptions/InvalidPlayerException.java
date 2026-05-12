49ea25066abcdc9b8762e977c381cec69220f9a2
Felix Andersson <felix.andersson@gmail.com>
2026-05-12 09:24:31 +0200

    added execption documentation

--- src/main/java/se/yrgo/exceptions/InvalidPlayerException.java
+++ src/main/java/se/yrgo/exceptions/InvalidPlayerException.java
@@ -1,6 +1,11 @@
 package se.yrgo.exceptions;
 
+/**
+ * Exception thrown when invalid player data is provided,
+ * for example invalid ratings or jersey numbers.
+ */
 public class InvalidPlayerException extends RuntimeException {
+
     public InvalidPlayerException(String message) {
         super(message);
     }
--- src/main/java/se/yrgo/exceptions/PlayerNotFoundException.java
+++ src/main/java/se/yrgo/exceptions/PlayerNotFoundException.java
@@ -1,5 +1,9 @@
 package se.yrgo.exceptions;
 
+/**
+ * Exception thrown when a player with the given id
+ * could not be found in the database.
+ */
 public class PlayerNotFoundException extends Exception {
 
     public PlayerNotFoundException(int playerId) {
